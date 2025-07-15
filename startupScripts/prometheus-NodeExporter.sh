0f3bb2e01ec644b6cf156798e5ffe27903927113
Jon <121370426+JonmarCorpuz@users.noreply.github.com>
2025-07-14 22:34:22 -0400

    Update prometheus-NodeExporter.sh

--- startupScripts/prometheus-NodeExporter.sh
+++ startupScripts/prometheus-NodeExporter.sh
@@ -2,10 +2,44 @@
 
 # ==== INSTALL NODE EXPORTER ==================================================================================================================================================
 
-wget https://github.com/prometheus/node_exporter/releases/download/v*/node_exporter-*.*-amd64.tar.gz
-tar xvfz node_exporter-*.*-amd64.tar.gz
+sudo apt update
+sudo apt install -y curl tar
+
+# Fetch the latest version of the node exporter
+LATEST=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep '"tag_name":' | cut -d '"' -f4 | sed 's/^v//')
+
+# Install the latest version of the node exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v${LATEST}/node_exporter-${LATEST}.linux-amd64.tar.gz
+tar -xvzf node_exporter-*.*-amd64.tar.gz
 cd node_exporter-*.*-amd64
-./node_exporter
+
+# Create a systemd service for the node_exporter
+sudo useradd --no-create-home --shell /bin/false node_exporter &> /dev/null
+sudo useradd --no-create-home --shell /bin/false node_exporter &> /dev/null
+sudo chown -R node_exporter:node_exporter /opt/node_exporter 
+sudo mkdir /opt/node_exporter
+sudo mv node_exporter /opt/node_exporter/
+
+sudo su -c 'echo """
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/opt/node_exporter/node_exporter
+
+[Install]
+WantedBy=default.target
+""" > /etc/systemd/system/node_exporter.service'
+
+# Enable and start the node_exporter
+sudo systemctl daemon-reexec &> /dev/null
+sudo systemctl daemon-reload &> /dev/null
+sudo systemctl enable node_exporter &> /dev/null
+sudo systemctl start node_exporter &> /dev/null
 
 # ==== REFERENCES =============================================================================================================================================================
 
